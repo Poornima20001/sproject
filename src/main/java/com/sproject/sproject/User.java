beab46a0c839c0be3acacdf39ce5bf89e61aebbb
Poornima S <stpoornima2000@gmail.com>
2026-07-25 12:17:05 +0530

    Update User.java

--- src/main/java/com/sproject/sproject/User.java
+++ src/main/java/com/sproject/sproject/User.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 @Entity
+	@Table(name = "users")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)	
